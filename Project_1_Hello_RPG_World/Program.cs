ad4e9424126ce7988b4b06cd3148be6a09969552
stefankarlsson1230@gmail.com <stefankarlsson1230@gmail.com>
2025-06-25 09:59:51 +0200

    Added checkboxes to the ReadMe.

--- Project_1_Hello_RPG_World/Program.cs
+++ Project_1_Hello_RPG_World/Program.cs
@@ -1,1 +1,5 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Console app that prints a welcome message.
+//User enters their name and chooses a character class.
+//Select class via menu.
+//Extra: ASCII art logo.
+
